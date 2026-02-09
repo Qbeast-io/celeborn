1bb42977e702549ca05eead7ff487221aa8cf417
Enrico Olivelli <eolivelli@gmail.com>
2026-02-09 10:55:22 +0100

    Remove uwanted file

--- release_spark_client_github.sh
+++
@@ -1,3 +0,0 @@
-set -x
-REPO=https://maven.pkg.github.com/qbeast-io/celeborn
-mvn deploy -DskipTests -Paws -Pspark-3.5 -DdistMgmtReleaseId=github -DdistMgmtReleaseName=github  -DdistMgmtReleaseUrl=$REPO -DdistMgmtSnapshotsId=github -DdistMgmtSnapshotsName=github -DdistMgmtSnapshotsUrl=$REPO
